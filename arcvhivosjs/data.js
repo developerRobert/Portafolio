5fc52885d827e559444773637f5aefeb9672a7f8
Robert <Robert.gitlab23@gmail.com>
2023-04-25 22:13:20 -1200

    agergando estilo y funcionalida al portafolio

---
+++ arcvhivosjs/data.js
@@ -0,0 +1,56 @@
+
+
+export const arrayProject=[
+    {
+    id:1,
+    descripcion:" hola mundo",
+    name:"ToDo",
+    link:"hola",
+    imagen:`./Img/todo.jpg`,
+    category:"app"
+    },
+    {
+    id:2,
+    descripcion:" hola mundo",
+    name:"Galery",
+    link:"hola",
+    imagen:"./Img/galery1.jpg",
+    category:"web"
+    },
+    {
+    id:3,
+    descripcion:" hola mundo",
+    name:"CRUD",
+    link:"hola",
+    imagen:"./Img/crud.jpg",
+    category:"app"
+    },
+    {
+    id:4,
+    descripcion:" hola mundo",
+    name:"Ecomerce",
+    link:"hola",
+    imagen:"./Img/ecommerce.jpg",
+    category:"app"
+    },
+    {
+    id:5,
+    descripcion:" hola mundo",
+    name:"Holy Panaderia",
+    link:"hola",
+    imagen:"./Img/holy.jpeg",
+    category:"app"
+    },
+    {
+    id:6,
+    descripcion:" hola mundo",
+    name:" Calculadora",
+    link:"hola",
+    imagen:"./Img/calculadora1.jpg",
+    category:"app"
+    },
+    
+
+
+
+]
--- arcvhivosjs/index.js
+++ arcvhivosjs/index.js
@@ -1,3 +1,6 @@
+import { arrayProject } from "./data.js";
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -47,6 +50,55 @@ document.addEventListener("DOMContentLoaded", () => {
         activeItem();
 
     })
+
+// ____________________________________________________________________
+ // filtro de proyectos
+    const contenedor=document.querySelector(".section_project_card")
+    const btns=document.querySelectorAll(".btns")  
+
+    function filtro(array) {
+        if (array.length === 0) {
+            contenedor.innerText = " No se encontro proyectos ";
+        }else{
+            contenedor.innerHTML="";
+        }
+
+        array.map((value)=>{
+            const card=document.createElement("DIV");
+            card.innerHTML=`
+            <div class="project_cards">
+            <img src=${value.imagen} class="cards_img">
+            <h3 class="project_cards_titulo">${value.name}</h3>
+            <div class="project_cards_infor">
+                <p class="project_cards_text">${value.descripcion}</p>
+                <a href=${value.link}  class="project_cards_btn ">See</a>
+               
+            </div>
+        </div> 
+            `
+
+            contenedor.appendChild(card);
+        })
+    }
+    filtro(arrayProject);
+
+    for (let i = 0; i < btns.length; i++) {
+        // console.log(i)
+        btns[i].addEventListener("click",(e)=>{
+            let show= e.target.innerText.toLowerCase();
+            if (show === "all") {
+                filtro(arrayProject)
+            }else{
+                let fil=arrayProject.filter((valor)=>(valor.category.toLowerCase() === show ))
+                filtro(fil)
+            }
+            
+
+        })
+        
+    }
+
+   
 
 
 
